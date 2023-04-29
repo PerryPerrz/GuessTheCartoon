f8853d18f32eafd95eb9c29ff1c7daa2309fbaac
PerryPerrz <iophugstud6@gmail.com>
2023-04-29 13:10:36 +0200

    The Game

---
+++ index.js
@@ -0,0 +1,126 @@
+const inputs = document.querySelector(".word"),
+    hintTag = document.querySelector(".hint span"),
+    guessLeft = document.querySelector(".guess span"),
+    mistakes = document.querySelector(".wrong span"),
+    resetBtn = document.querySelector(".reset"),
+    hintBtn = document.querySelector(".showhint"),
+    hintElement = document.querySelector(".hint"),
+    typeInput = document.querySelector(".type-input");
+
+// Initializing game variables
+let word, incorrectLetters = [], correctLetters = [], maxGuesses;
+let unalphabeticalChar = 0;
+
+// Select random word from word list and setting up the game
+function startGame() {
+    alert("New Game Started ! Guess new word");
+
+    unalphabeticalChar = 0;
+    incorrectLetters = [];
+    correctLetters = [];
+
+    // Hide hint element
+    hintElement.style.display = "none";
+    hintElement.style.opacity = "0";
+
+    // Choose random word from word list and set up game
+    const randWord = cartoonList[Math.floor(Math.random() * cartoonList.length)];
+    word = randWord.word;
+
+    maxGuesses = word.length >= 5 ? 8 : 6;
+
+    // Display hint and game stats
+    hintTag.innerText = randWord.hint;
+    guessLeft.innerText = maxGuesses;
+    mistakes.innerText = incorrectLetters;
+
+    // Create input for each letter
+    inputs.innerHTML = "";
+    for (let i = 0; i < word.length; i++) {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.disabled = true;
+
+        // If the letter is a space, comma, period, hyphen, or apostrophe, fill it in immediately
+        if (word[i] === "," || word[i] === "." || word[i] === "-" || word[i] === "'") {
+            input.value = word[i];
+            unalphabeticalChar++;
+        }
+
+        // If it's a space, I don't print de border of the input
+        if (word[i] === " ") {
+            input.style.border = "none";
+            unalphabeticalChar++;
+        }
+
+        // Add input to DOM
+        inputs.appendChild(input);
+    }
+}
+
+// Handle user input and update game
+function handleInput(e) {
+
+    // Ignore non-alphabetical characters and letters that have already been guessed
+    const key = e.target.value.toLowerCase();
+    if (key.match(/^[a-z]+$/i) && !incorrectLetters.includes(`${key}`) && !correctLetters.includes(`${key}`)) {
+
+        // Update correct guess
+        if (word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] === key) {
+                    inputs.querySelectorAll("input")[i].value += key;
+                }
+            }
+
+            // if the words contain more than one time the letter, I need to add it more than one time in the list
+            if (word.split(key).length - 1 > 1) {
+                for (let i = 0; i < word.split(key).length - 1; i++) {
+                    correctLetters += key;
+                }
+            } else {
+                correctLetters += key;
+            }
+        } else {
+            // Update incorrect guess
+            maxGuesses--;
+            incorrectLetters.push(`${key}`);
+            mistakes.innerText = incorrectLetters;
+        }
+    }
+
+    // Update remain guess and check for win/lose conditions
+    guessLeft.innerText = maxGuesses;
+    console.log(correctLetters.length, word.length);
+    console.log(unalphabeticalChar);
+    if (correctLetters.length === word.length - unalphabeticalChar) {
+        alert(`You found the word ! It was ${word.toUpperCase()}`);
+        startGame();
+    } else if (maxGuesses < 1) {
+        alert("You lost ! You don't have remaining guesses");
+
+        for (let i = 0; i < word.length; i++) {
+            // Fill inputs with correct words
+            inputs.querySelectorAll("input")[i].value = word[i];
+        }
+    }
+
+    // Clear input field after each guess
+    typeInput.value = "";
+}
+
+// Show hint element
+function showHintElement() {
+    hintElement.style.display = "block";
+    hintElement.style.opacity = "1";
+}
+
+// Setup event listeners
+resetBtn.addEventListener("click", startGame);
+hintBtn.addEventListener("click", showHintElement);
+typeInput.addEventListener("input", handleInput);
+inputs.addEventListener("click", () => typeInput.focus());
+document.addEventListener("keydown", () => typeInput.focus());
+
+// Start game
+startGame();
---
+++ words-db.js
@@ -0,0 +1,82 @@
+let cartoonList = [
+  {
+    word: "astro,lepetitrobot",
+    hint: "Les voilà, les voilà, les heros de l'espace"
+  },
+  {
+    word: "les chevaliers du zodiaque",
+    hint: "Proteger la deesse Athena et vaincre tous les ennemis"
+  },
+  {
+    word: "les mysterieuses cites d'or",
+    hint: "Esteban, Zia, Tao, les enfants partent pour un long voyage"
+  },
+  {
+    word: "inspecteur gadget",
+    hint: "Go Go Gadget au fin fond des mers"
+  },
+  {
+    word: "goldorak",
+    hint: "Va combattre tes ennemis, Golgoths, va combattre sans repit"
+  },
+  {
+    word: "les schtroumpfs",
+    hint: "La la la la la la, on schtroumpfe tous ensemble"
+  },
+  {
+    word: "les razmoket",
+    hint: "Les Razmoket, les Razmoket, ils font des bêtises partout"
+  },
+  {
+    word: "olive et tom",
+    hint: "Allez les champions, allez, allez, allez"
+  },
+  {
+    word: "les entrechats",
+    hint: "Les Entrechats, les Entrechats, toujours prêts pour un nouveau combat"
+  },
+  {
+    word: "il etait une fois...l'homme",
+    hint: "Il etait une fois... l'Homme, il etait une fois... la vie"
+  },
+  {
+    word: "pokemon",
+    hint: "Attrapez-les tous !"
+  },
+  {
+    word: "dragonball",
+    hint: "Dragon ! Dragon ! Prends ton envol !"
+  },
+  {
+    word: "sailormoon",
+    hint: "Envoûtee par la lune mystique, je vais punir les mechants !"
+  },
+  {
+    word: "tom et jerry",
+    hint: "It's the cat and mouse game we love to play"
+  },
+  {
+    word: "tintin",
+    hint: "Tintin et Milou, partent toujours pour de nouveaux horizons"
+  },
+  {
+    word: "les minipouss",
+    hint: "Nous sommes les Minipouss, des petits êtres d'un centimètre de haut"
+  },
+  {
+    word: "les mysterieuses affaires de tobie",
+    hint: "Un problème à resoudre, une enigme à denouer, Tobie est là !"
+  },
+  {
+    word: "maya l'abeille",
+    hint: "Avec Maya l'abeille, on apprend tout en s'amusant"
+  },
+  {
+    word: "les muppets show",
+    hint: "C'est le Muppet Show, avec leur drôle de decor"
+  },
+  {
+    word: "les castors allumes",
+    hint: "Les Castors allumes, ils sont frappadingues"
+  }
+];  
