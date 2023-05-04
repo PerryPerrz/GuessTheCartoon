943a1570176deb73a0f0aaf966a3c4a95df51f40
PerryPerrz <iophugstud6@gmail.com>
2023-05-04 10:18:28 +0200

    Update path

--- words-db.js
+++ words-db.js
@@ -2,191 +2,191 @@ let cartoonList = [
   {
     word: "astro,lepetitrobot",
     hint: "Les voilà, les voilà, les heros de l'espace",
-    audio: "/assets/audio/astro-lepetitrobot.mp3"
+    audio: "assets/audio/astro-lepetitrobot.mp3"
   },
   {
     word: "les chevaliers du zodiaque",
     hint: "Proteger la deesse Athena et vaincre tous les ennemis",
-    audio: "/assets/audio/les-chevaliers-du-zodiaque.mp3"
+    audio: "assets/audio/les-chevaliers-du-zodiaque.mp3"
   },
   {
     word: "les mysterieuses cites d'or",
     hint: "Esteban, Zia, Tao, les enfants partent pour un long voyage",
-    audio: "/assets/audio/les-mysterieuses-cites-dor.mp3"
+    audio: "assets/audio/les-mysterieuses-cites-dor.mp3"
   },
   {
     word: "inspecteur gadget",
     hint: "Go Go Gadget au fin fond des mers",
-    audio: "/assets/audio/inspecteur-gadget.mp3"
+    audio: "assets/audio/inspecteur-gadget.mp3"
   },
   {
     word: "goldorak",
     hint: "Va combattre tes ennemis, Golgoths, va combattre sans repit",
-    audio: "/assets/audio/goldorak.mp3"
+    audio: "assets/audio/goldorak.mp3"
   },
   {
     word: "les schtroumpfs",
     hint: "La la la la la la, on schtroumpfe tous ensemble",
-    audio: "/assets/audio/les-schtroumpfs.mp3"
+    audio: "assets/audio/les-schtroumpfs.mp3"
   },
   {
     word: "les razmoket",
     hint: "Les Razmoket, les Razmoket, ils font des bêtises partout",
-    audio: "/assets/audio/les-razmoket.mp3"
+    audio: "assets/audio/les-razmoket.mp3"
   },
   {
     word: "olive et tom",
     hint: "Allez les champions, allez, allez, allez",
-    audio: "/assets/audio/olive-et-tom.mp3"
+    audio: "assets/audio/olive-et-tom.mp3"
   },
   {
     word: "les entrechats",
     hint: "Les Entrechats, les Entrechats, toujours prêts pour un nouveau combat",
-    audio: "/assets/audio/les-entrechats.mp3"
+    audio: "assets/audio/les-entrechats.mp3"
   },
   {
     word: "il etait une fois...l'homme",
     hint: "Il etait une fois... l'Homme, il etait une fois... la vie",
-    audio: "/assets/audio/il-etait-une-fois-lhomme.mp3"
+    audio: "assets/audio/il-etait-une-fois-lhomme.mp3"
   },
   {
     word: "pokemon",
     hint: "Attrapez-les tous !",
-    audio: "/assets/audio/pokemon.mp3"
+    audio: "assets/audio/pokemon.mp3"
   },
   {
     word: "dragonball",
     hint: "Dragon ! Dragon ! Prends ton envol !",
-    audio: "/assets/audio/dragonball.mp3"
+    audio: "assets/audio/dragonball.mp3"
   },
   {
     word: "sailor moon",
     hint: "Envoûtee par la lune mystique, je vais punir les mechants !",
-    audio: "/assets/audio/sailor-moon.mp3"
+    audio: "assets/audio/sailor-moon.mp3"
   },
   {
     word: "tintin",
     hint: "Tintin et Milou, partent toujours pour de nouveaux horizons",
-    audio: "/assets/audio/tintin.mp3"
+    audio: "assets/audio/tintin.mp3"
   },
   {
     word: "les minipouss",
     hint: "Nous sommes les Minipouss, des petits êtres d'un centimètre de haut",
-    audio: "/assets/audio/les-minipouss.mp3"
+    audio: "assets/audio/les-minipouss.mp3"
   },
   {
     word: "maya l'abeille",
     hint: "Avec Maya l'abeille, on apprend tout en s'amusant",
-    audio: "/assets/audio/maya-labeille.mp3"
+    audio: "assets/audio/maya-labeille.mp3"
   },
   {
     word: "beyblade metal fusion",
     hint: "HYPER VITESSE !",
-    audio: "/assets/audio/beyblade-metal-fusion.mp3"
+    audio: "assets/audio/beyblade-metal-fusion.mp3"
   },
   {
     word: "clifford le gros chien rouge",
     hint: "Il est vraiment génial",
-    audio: "/assets/audio/clifford-le-gros-chien-rouge.mp3"
+    audio: "assets/audio/clifford-le-gros-chien-rouge.mp3"
   },
   {
     word: "denver le dernier dinosaure",
     hint: "C'est mon ami et bien plus encore",
-    audio: "/assets/audio/denver-le-dernier-dinosaure.mp3"
+    audio: "assets/audio/denver-le-dernier-dinosaure.mp3"
   },
   {
     word: "galactik football",
     hint: "C'est le football du futur",
-    audio: "/assets/audio/galactik-football.mp3"
+    audio: "assets/audio/galactik-football.mp3"
   },
   {
     word: "l'agence tous risques",
     hint: "Si vous avez un problème, si personne ne peut vous aider, appelez nous",
-    audio: "/assets/audio/lagence-tous-risques.mp3"
+    audio: "assets/audio/lagence-tous-risques.mp3"
   },
   {
     word: "les dalton",
     hint: "4 frères, 4 bandits",
-    audio: "/assets/audio/les-dalton.mp3"
+    audio: "assets/audio/les-dalton.mp3"
   },
   {
     word: "linus et boom",
     hint: "Un garçon et un alien rouge",
-    audio: "/assets/audio/linus-et-boom.mp3"
+    audio: "assets/audio/linus-et-boom.mp3"
   },
   {
     word: "titi et grosminet",
     hint: "Je t'aurai un jour, je t'aurai",
-    audio: "/assets/audio/titi-et-grosminet.mp3"
+    audio: "assets/audio/titi-et-grosminet.mp3"
   },
   {
     word: "george de la jungle",
     hint: "George, George, George de la jungle",
-    audio: "/assets/audio/george-de-la-jungle.mp3"
+    audio: "assets/audio/george-de-la-jungle.mp3"
   },
   {
     word: "inazuma eleven",
     hint: "C'est le football enflammé",
-    audio: "/assets/audio/inazuma-eleven.mp3"
+    audio: "assets/audio/inazuma-eleven.mp3"
   },
   {
     word: "jimmy neutron",
     hint: "Un petit génie",
-    audio: "/assets/audio/jimmy-neutron.mp3"
+    audio: "assets/audio/jimmy-neutron.mp3"
   },
   {
     word: "johnny test",
     hint: "Johnny Test, Johnny Test, Johnny Test",
-    audio: "/assets/audio/johnny-test.mp3"
+    audio: "assets/audio/johnny-test.mp3"
   },
   {
     word: "les ratz",
     hint: "Les animaux favoris d'Éric et Ramzy",
-    audio: "/assets/audio/les-ratz.mp3"
+    audio: "assets/audio/les-ratz.mp3"
   },
   {
     word: "les zinzins de l'espace",
     hint: "Candy, Etno, Gorgious, Bud et Stereo",
-    audio: "/assets/audio/les-zinzins-de-lespace.mp3"
+    audio: "assets/audio/les-zinzins-de-lespace.mp3"
   },
   {
     word: "marcelino pan y vino",
     hint: "Un enfant abandonné, recueilli par des moines",
-    audio: "/assets/audio/marcelino-pan-y-vino.mp3"
+    audio: "assets/audio/marcelino-pan-y-vino.mp3"
   },
   {
     word: "mon ami marsupilami",
     hint: "Il est jaune avec des taches noires OUBA OUBA",
-    audio: "/assets/audio/mon-ami-marsupilami.mp3"
+    audio: "assets/audio/mon-ami-marsupilami.mp3"
   },
   {
     word: "pere castor",
     hint: "Raconte-nous une histoire",
-    audio: "/assets/audio/pere-castor.mp3"
+    audio: "assets/audio/pere-castor.mp3"
   },
   {
     word: "quoi d'neuf scooby-doo",
     hint: "Résoudre des mystères, c'est notre spécialité",
-    audio: "/assets/audio/quoi-dneuf-scooby-doo.mp3"
+    audio: "assets/audio/quoi-dneuf-scooby-doo.mp3"
   },
   {
     word: "shuriken school",
     hint: "C'est l'école des ninjas",
-    audio: "/assets/audio/shuriken-school.mp3"
+    audio: "assets/audio/shuriken-school.mp3"
   },
   {
     word: "simsala grimm",
     hint: "Yoyo et Doc Croc",
-    audio: "/assets/audio/simsala-grimm.mp3"
+    audio: "assets/audio/simsala-grimm.mp3"
   },
   {
     word: "slugterra",
     hint: "Des limaces qui se transforment en armes",
-    audio: "/assets/audio/slugterra.mp3"
+    audio: "assets/audio/slugterra.mp3"
   },
   {
     word: "code lyoko",
     hint: "Un monde virtuel (sans danger) pour sauver le monde réel",
-    audio: "/assets/audio/code-lyoko.mp3"
+    audio: "assets/audio/code-lyoko.mp3"
   },
 ];
